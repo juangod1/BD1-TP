c50af463eebcfcb295652ae42960604a0f55f9d9
Diego Bruno <dbruno21@bitbucket.org>
2018-06-09 23:48:03 -0300

    migracion(). Faltan las validaciones antes de cada insercion

---
+++ scripts/funciones.sql
@@ -0,0 +1,97 @@
+-- Inicializacion --
+
+DROP TABLE recorrido_final;
+DROP TABLE aux;
+
+
+CREATE TABLE recorrido_final
+(
+  	periodo TEXT,
+  	usuario INTEGER,
+  	fecha_hora_ret TIMESTAMP NOT NULL,
+  	est_origen INTEGER NOT NULL,
+  	est_destino INTEGER NOT NULL,
+  	fecha_hora_dev TIMESTAMP NOT NULL CHECK(fecha_hora_dev>=fecha_hora_ret),
+  	PRIMARY KEY(usuario,fecha_hora_ret)
+);
+
+
+CREATE TABLE aux 
+(
+	periodo TEXT,
+  	id_usuario TEXT,
+  	fecha_hora_retiro TEXT,
+  	origen_estacion TEXT,
+  	nombre_origen TEXT,
+  	destino_estacion TEXT,
+  	nombre_destino TEXT,
+  	tiempo_uso TEXT,
+  	fecha_creacion TEXT
+);
+
+
+\copy aux FROM test1.csv csv header delimiter ';'
+
+
+-- Migracion --
+
+CREATE OR REPLACE FUNCTION migracion() 
+RETURNS VOID AS $$
+DECLARE
+	aRec RECORD;
+	cursor CURSOR FOR SELECT * FROM aux;
+BEGIN
+ 	OPEN cursor;
+ 	LOOP
+ 		FETCH cursor INTO aRec;
+ 		EXIT WHEN NOT FOUND;
+
+ 		PERFORM insertar_recorrido(aRec.periodo, CAST(aRec.id_usuario AS INTEGER), CAST(aRec.fecha_hora_retiro AS TIMESTAMP), CAST(aRec.origen_estacion AS INTEGER), CAST(aRec.destino_estacion AS INTEGER), CAST(aRec.fecha_hora_retiro AS TIMESTAMP) + CAST(replace(replace(replace(aRec.tiempo_uso, 'H ', 'H'), 'MIN ', 'M'), 'SEG', 'S') AS INTERVAL));
+ 	END LOOP;
+ 	CLOSE cursor;
+
+ 	DROP TABLE aux;
+ 	PERFORM agregar_trigger();
+END;
+$$ LANGUAGE PLPGSQL;
+
+
+CREATE OR REPLACE FUNCTION insertar_recorrido(periodo TEXT, usuario INTEGER, fecha_hora_ret TIMESTAMP, est_origen INTEGER, est_destino INTEGER, fecha_hora_dev TIMESTAMP) 
+RETURNS VOID AS $$
+BEGIN
+	-- Aca van las validaciones
+	INSERT INTO recorrido_final VALUES(periodo, usuario, fecha_hora_ret, est_origen, est_destino, fecha_hora_dev);
+EXCEPTION
+	WHEN OTHERS THEN
+		raise notice '% %', SQLSTATE, SQLERRM;
+END;
+$$ LANGUAGE PLPGSQL;
+
+
+-- Trigger --
+
+CREATE OR REPLACE FUNCTION agregar_trigger() 
+RETURNS VOID AS $$
+BEGIN
+	CREATE TRIGGER detecta_solapado BEFORE INSERT ON RECORRIDO_FINAL
+	FOR EACH ROW
+	EXECUTE PROCEDURE detecta_solapado();
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION detecta_solapado() 
+RETURNS Trigger AS $$
+DECLARE
+ 	countSolapados INT;
+BEGIN
+	SELECT count(*) INTO countSolapados
+	FROM RECORRIDO_FINAL
+	WHERE usuario = new.usuario AND (fecha_hora_ret <= new.fecha_hora_dev) AND (fecha_hora_dev >= new.fecha_hora_ret);
+
+	IF (countSolapados > 0) THEN
+		RAISE EXCEPTION 'INSERCION IMPOSIBLE POR SOLAPAMIENTO';
+	END IF;
+
+	RETURN new;
+END;
+$$ LANGUAGE plpgsql;
