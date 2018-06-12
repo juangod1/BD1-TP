f1e6f0cbba285350aeabf0fdcc69b28e009fbac1
juangod1 <juan.godfrid@gmail.com>
2018-06-12 01:46:48 -0300

    falta terminar solapados encadenados

--- scripts/funciones.sql
+++ scripts/funciones.sql
@@ -16,7 +16,7 @@ CREATE TABLE recorrido_final
 );
 
 
-CREATE TABLE aux 
+CREATE TABLE aux
 (
 	periodo TEXT,
   	id_usuario TEXT,
@@ -35,16 +35,57 @@ CREATE TABLE aux
 
 -- Migracion --
 
-CREATE OR REPLACE FUNCTION migracion() 
+CREATE OR REPLACE FUNCTION migracion()
 RETURNS VOID AS $$
 DECLARE
 	aRec RECORD;
 	cursor CURSOR FOR SELECT * FROM aux;
+  tuple RECORD;
+  match RECORD;
 BEGIN
  	OPEN cursor;
+
+  -------------------------------------
+  -- usuario + fecha_hora_ret unicos --
+  -------------------------------------
+  FOR tuple IN select * from aux LOOP
+    CREATE TABLE matches AS SELECT * FROM aux WHERE tuple.id_usuario + tuple.fecha_hora_retiro = aux.id_usuario + aux.fecha_hora_retiro ORDER BY tiempo_uso;
+
+    FOR match IN SELECT * FROM matches LIMIT 1 LOOP
+      DELETE FROM aux match;
+    END LOOP;
+
+    FOR match IN SELECT * FROM matches OFFSET 2 LOOP
+      DELETE FROM aux match;
+    END LOOP;
+
+    DROP TABLE matches;
+  END LOOP;
+  -------------------------------------
+
  	LOOP
  		FETCH cursor INTO aRec;
  		EXIT WHEN NOT FOUND;
+
+    --------------------------------------
+    -- Campos no nulos y tiempo natural --
+    --------------------------------------
+    IF aRec.id_usuario IS NULL THEN
+      CONTINUE;
+    END IF;
+    IF aRec.fecha_hora_retiro IS NULL THEN
+      CONTINUE;
+    END IF;
+    IF aRec.origen_estacion IS NULL THEN
+      CONTINUE;
+    END IF;
+    IF aRec.destino_estacion IS NULL THEN
+      CONTINUE;
+    END IF;
+    IF aRec.tiempo_uso IS NULL OR aRec.tiempo_uso<0 OR NOT isnumeric(aRec.tiempo_uso) THEN
+      CONTINUE;
+    END IF;
+    -------------------------------------
 
  		PERFORM insertar_recorrido(aRec.periodo, CAST(aRec.id_usuario AS INTEGER), CAST(aRec.fecha_hora_retiro AS TIMESTAMP), CAST(aRec.origen_estacion AS INTEGER), CAST(aRec.destino_estacion AS INTEGER), CAST(aRec.fecha_hora_retiro AS TIMESTAMP) + CAST(replace(replace(replace(aRec.tiempo_uso, 'H ', 'H'), 'MIN ', 'M'), 'SEG', 'S') AS INTERVAL));
  	END LOOP;
@@ -56,21 +97,49 @@ END;
 $$ LANGUAGE PLPGSQL;
 
 
-CREATE OR REPLACE FUNCTION insertar_recorrido(periodo TEXT, usuario INTEGER, fecha_hora_ret TIMESTAMP, est_origen INTEGER, est_destino INTEGER, fecha_hora_dev TIMESTAMP) 
-RETURNS VOID AS $$
+-- https://stackoverflow.com/questions/16195986/isnumeric-with-postgresql --
+CREATE OR REPLACE FUNCTION isnumeric(text) RETURNS BOOLEAN AS $$
+DECLARE x NUMERIC;
 BEGIN
-	-- Aca van las validaciones
-	INSERT INTO recorrido_final VALUES(periodo, usuario, fecha_hora_ret, est_origen, est_destino, fecha_hora_dev);
-EXCEPTION
+    x = $1::NUMERIC;
+    RETURN TRUE;
+EXCEPTION WHEN others THEN
+    RETURN FALSE;
+END;
+$$
+STRICT
+LANGUAGE plpgsql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION insertar_recorrido(periodo TEXT, usuario INTEGER, fecha_hora_ret TIMESTAMP, est_origen INTEGER, est_destino INTEGER, fecha_hora_dev TIMESTAMP)
+RETURNS VOID AS $$
+DECLARE
+tuple RECORD;
+BEGIN
+  ---------------------------
+  -- solapados encadenados --
+  ---------------------------
+  FOR tuple IN SELECT * FROM recorrido_final LOOP
+    IF usuario = tuple.usuario AND (fecha_hora_ret <= tuple.fecha_hora_dev) AND (fecha_hora_dev >= tuple.fecha_hora_ret) THEN
+      IF fecha_hora_retiro<=tuple.fecha_hora_retiro THEN
+      
+      ELSE THEN
+
+      END IF;
+    END IF;
+  END LOOP;
+  ---------------------------
+
+  INSERT INTO recorrido_final VALUES(periodo, usuario, fecha_hora_ret, est_origen, est_destino, fecha_hora_dev);
+  EXCEPTION
 	WHEN OTHERS THEN
 		raise notice '% %', SQLSTATE, SQLERRM;
-END;
+    END;
 $$ LANGUAGE PLPGSQL;
 
 
 -- Trigger --
 
-CREATE OR REPLACE FUNCTION agregar_trigger() 
+CREATE OR REPLACE FUNCTION agregar_trigger()
 RETURNS VOID AS $$
 BEGIN
 	CREATE TRIGGER detecta_solapado BEFORE INSERT ON RECORRIDO_FINAL
@@ -79,7 +148,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION detecta_solapado() 
+CREATE OR REPLACE FUNCTION detecta_solapado()
 RETURNS Trigger AS $$
 DECLARE
  	countSolapados INT;
